7c9c9bbd2c79fccdfbb715d412a273933c6dbcdb
Brandon Tjandra <btjandra15@gmail.com>
2024-10-04 21:52:46 -0400

    Added chart to net worth card & started asset/transaction card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,19 @@
 "use client"
 
 import Sidebar from '@/components/Sidebar';
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import React, { useState } from 'react'
+import { Bar, BarChart, CartesianGrid, Tooltip, XAxis } from 'recharts';
 
 const Dashboard = () => {
-    const cateogries = [
-        { name: "Accounts", percantage: "14%" },
-        { name: "Stocks", percantage: "14%" },
-        { name: "Crypto", percantage: "14%" },
-        { name: "Real Estate", percantage: "14%" },
-        { name: "Cars", percantage: "14%" },
-        { name: "Liabilites", percantage: "14%" },
-        { name: "Other", percantage: "14%" },
+    const categories = [
+        { name: "Accounts", percentage: 25, color: "#3d84f7" },
+        { name: "Stocks", percentage: 15, color: "#3d84f7"},
+        { name: "Crypto", percentage: 20, color: "#3d84f7" },
+        { name: "Real Estate", percentage: 10, color: "#3d84f7" },
+        { name: "Cars", percentage: 5, color: "#3d84f7"},
+        { name: "Liabilites", percentage: 15, color: "#3d84f7" },
+        { name: "Other", percentage: 10, color: "#3d84f7"},
     ]
 
     const chartData = [
@@ -21,14 +23,39 @@ const Dashboard = () => {
         { month: "April", net_worth: 73, assets: 190 },
         { month: "May", net_worth: 209, assets: 130 },
         { month: "June", net_worth: 214, assets: 140 },
+        { month: "July", net_worth: 186, assets: 80 },
+        { month: "August", net_worth: 305, assets: 200 },
+        { month: "September", net_worth: 237, assets: 120 },
+        { month: "October", net_worth: 73, assets: 190 },
+        { month: "November", net_worth: 209, assets: 130 },
+        { month: "December", net_worth: 214, assets: 140 },
     ]
 
-    // Dummy data for Testing
+    const assets = [
+        { account: 'Barclays 1948', change: 500.0, cost: 1000.0, value: 1500.0 },
+        { account: 'Santander 1511', change: -300.0, cost: 800.0, value: 500.0 },
+        { account: 'AAPL 2 shares', change: 200.0, cost: 400.0, value: 600.0 },
+      ];
+
     const transactions = [
-        { amount: '+$500.00', account: 'Barclays 1948', date: '3 Jan 15:41' },
-        { amount: '+$4300.00', account: 'Barclays 1948', date: '2 Jan 20:53' },
-        { amount: '-$500.00', account: 'Santander 1511', date: '1 Jan 11:09' },
+        { amount: 500.0, detail: 'Account Barclays 1948', date: '3 Jan', time: '15:41' },
+        { amount: 4300.0, detail: 'Account Barclays 1948', date: '2 Jan', time: '20:53' },
+        { amount: -500.0, detail: 'Account Santander 1511', date: '1 Jan', time: '11:09' },
+        { amount: 435.41, detail: 'AAPL 2 shares', date: '30 Dec', time: '19:31' },
+        { amount: 500.0, detail: 'Barclays 1948 to Santander 1511', date: '23 Dec', time: '16:41' },
+        { amount: 500.0, detail: 'Account Barclays 1948', date: '13 Dec', time: '18:03' },
     ];
+
+    const chartConfig = {
+        net_worth: {
+            label: "Net Worth",
+            color: "#2563eb",
+        },
+        assets: {
+            label: "Assets",
+            color: "#60a5fa",
+        },
+    } satisfies ChartConfig
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -50,22 +77,48 @@ const Dashboard = () => {
                         <p className='text-green-400'>+543.42 (0.18%)</p>
 
                         {/* Graph */}
-                        <div className="mt-4 h-32 bg-gray-700 rounded-lg">
-
+                        <div className="mt-4 h-64 w-full rounded-lg">
+                            <ChartContainer config={chartConfig} className="h-full w-full">
+                                <BarChart accessibilityLayer data={chartData}>
+                                    <CartesianGrid vertical={false}/>
+                                    <XAxis dataKey="month" tickLine={false} tickMargin={10} axisLine={false} tickFormatter={(value) => value.slice(0, 3)}/>
+                                    <Tooltip content={<ChartTooltipContent/>}/>
+                                    <ChartLegend content={<ChartLegendContent/>}/>
+                                    <Bar dataKey="net_worth" fill='var(--color-net_worth)' radius={4}/>
+                                    <Bar dataKey="assets" fill='var(--color-assets)' radius={4}/>
+                                </BarChart>
+                            </ChartContainer>
                         </div>
                     </div>
 
-                    <div className="bg-dark-p-6 rounded-lg m-5">
-                        <h2 className='text-xl'>Cateogires</h2>
+                    {/* Cateogries */}
+                    <div className="bg-dark p-6 rounded-lg m-5">
+                        <h2 className="text-xl">Categories</h2>
 
-                        <ul className='mt-4 space-y-2'>
-                            {cateogries.map((cateogry, index) => {
-                                return(
-                                    <li className='flex justify-between' key={index}>
-                                        <span>{cateogry.name}</span>
-                                        <span>{cateogry.percantage}</span>
+                        <ul className="mt-4 space-y-4">
+                            {categories.map((category, index) => {
+                                return (
+                                    <li className="flex items-center justify-between space-x-2" key={index}>
+                                        {/* Category Name and Percentage */}
+                                        <div className="flex flex-col space-y-1 w-full">
+                                            <div className="flex justify-between">
+                                                <span className="text-sm">{category.name}</span>
+                                                <span className="text-sm">{category.percentage}%</span>
+                                            </div>
+
+                                            {/* Progress Bar */}
+                                            <div className="w-full bg-gray-700 rounded-full h-2.5">
+                                                <div
+                                                    className="h-2.5 rounded-full"
+                                                    style={{
+                                                        width: `${category.percentage}%`,
+                                                        backgroundColor: category.color,
+                                                    }}
+                                                />
+                                            </div>
+                                        </div>
                                     </li>
-                                )
+                                );
                             })}
                         </ul>
                     </div>
@@ -75,34 +128,43 @@ const Dashboard = () => {
                 <div className="grid grid-cols-2 gap-6 mt-8 ml-10 bg-car p-6 rounded-lg">
                     {/* Assets Card */}
                     <div className="bg-dark p-6 rounded-lg border-2 border-gray-500">
-                        <h2 className='text-xl'>Assets</h2>
-
-                        <div className="mt-4 space-y-4">
-                            <div className="flex justify-between">
-                                <span>Barclays</span>
-                                <span>$103,752</span>
-                            </div>
-
-                            <div className="flex justify-between">
-                                <span>Santander</span>
-                                <span>$11,920</span>
-                            </div>
-
-                            <div className="flex justify-between">
-                                <span>APPL</span>
-                                <span>$5,019</span>
-                            </div>
-
-                            <div className="flex justify-between">
-                                <span>MSFT</span>
-                                <span>$18,519</span>
-                            </div>
+                        <div className="flex justify-between items-center mb-4">
+                            <h2 className='text-xl font-semibold'>Assets</h2>
+                            <button className='text-gray-400 hover:text-white transition-colors'>View All</button>
                         </div>
+
+                        <div className="grid grid-cols-4 gap-4 text-gray-400 font-semibold mb-4">
+                            <div className="">Accounts</div>
+                            <div className="">Change</div>
+                            <div className="">Cost</div>
+                            <div className="">Value</div>
+                        </div>
+
+                        <ul className='space-y-4'>
+                            {assets.map((asset, index) => {
+                                return(
+                                    <li key={index} className='grid grid-cols-4 gap-4 text-gray-300'>
+                                        <div className="">{asset.account}</div>
+
+                                        <div className={`font-medium ${asset.change > 0 ? 'text-green-400' : "text-red-400"}`}>
+                                            {asset.change > 0 ? `+${asset.change}` : asset.change}
+                                        </div>
+
+                                        <div className="">{`$${asset.cost}`}</div>
+                                        <div className="">{`$${asset.value}`}</div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </div>
 
                     {/* Transactions Card */}
                     <div className="bg-dark p-6 rounded-lg border-2 border-gray-500">
-                      Test
+                        {/* Card Header */}
+                        <div className="flex justify-between items-center mb-4">
+                            <h2 className='text-xl font-semibold'>Transactions</h2>
+                            <button className='text-gray-400 hover:text-white transition-colors'>View All</button>
+                        </div>
                     </div>
                 </div>
             </div>
