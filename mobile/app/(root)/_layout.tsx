0fa5ec9fdfa7c87f938378ec6ba835f4c91da35f
Brandon Tjandra <btjandra15@gmail.com>
2025-08-19 15:45:24 -0400

    Added tabs navigation and started subscription tab

--- mobile/app/(root)/_layout.tsx
+++ mobile/app/(root)/_layout.tsx
@@ -1,10 +1,35 @@
 import { useUser } from "@clerk/clerk-expo";
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Stack, Tabs } from "expo-router";
 
 export default function Layout () {
     const {isSignedIn} = useUser();
 
     if(!isSignedIn) return <Redirect href={"/sign-in"}/>;
 
-    return <Stack screenOptions={{headerShown: false}}/>
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false,   
+                }}
+            />
+
+            <Tabs.Screen
+                name="create"
+                options={{
+                    href: null,
+                }}
+            />
+
+            <Tabs.Screen
+                name="subscriptions"
+                options={{
+                    title: 'Subscriptions',
+                    headerShown: false,   
+                }}
+            />
+        </Tabs>
+    )
 }
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,12 +1,14 @@
 import SafeScreen from '@/components/SafeScreen';
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
   return <ClerkProvider tokenCache={tokenCache}>
             <SafeScreen>
-              <Slot />
+              <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="(root)" options={{ headerShown: false }} />
+              </Stack>
             </SafeScreen>
           </ClerkProvider>;
 }
